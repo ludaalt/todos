88c8175b1bdff3c9c4634bf7c95355cf617294f2
Людмила Алтухова <luda.tamb@mail.ru>
2022-10-30 15:35:39 +0300

    feat: implement main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import { Typography } from '@mui/material';
+
+import TodosWrapper from './components/TodosWrapper';
 
 const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
   background-color: lightgray;
 `;
 
 function App() {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      <Typography variant='h1'>todos</Typography>
+      <TodosWrapper />
+    </MainContainer>
+  );
 }
 
 export default App;
